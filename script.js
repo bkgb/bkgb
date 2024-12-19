b8268149d14e31b575875b3dd21738fd7f1f0713
bkgb <born.king.ga.boy@gmail.com>
2024-12-19 04:03:35 -0500

    yes

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ fetch('links.json')
     });
   })
   .catch(error => console.error('Error loading links:', error));
+
+
+fetch('https://bkgb.github.io/data')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data fetched successfully:', data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
